2e078931c4a63465c319b6442d4e9d14ac4edc5f
spark <64788610@qq.com>
2025-09-24 16:33:44 +0800

    add common.rs

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rd.800688.xyz:50004".to_owned()
 }
 
 #[inline]
