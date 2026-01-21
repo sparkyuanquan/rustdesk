c84b3af2406f0fb77e0ef9089b48ef8e1be4220d
sparkyuanquan <50127057+sparkyuanquan@users.noreply.github.com>
2026-01-21 15:24:10 +0800

    Implement settings popup menu with hover effect
    Added a popup menu with a settings icon that responds to hover events.

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -256,8 +256,29 @@ class _DesktopHomePageState extends State<DesktopHomePage>
   }
 
 Widget buildPopupMenu(BuildContext context) {
-        return Container();
-      }
+       final textColor = Theme.of(context).textTheme.titleLarge?.color;
+                  RxBool hover = false.obs;
+                  return InkWell(
+                    onTap: DesktopTabPage.onAddSetting,
+                    child: Tooltip(
+                      message: translate('Settings'),
+                      child: Obx(
+                        () => CircleAvatar(
+                          radius: 15,
+                          backgroundColor: hover.value
+                              ? Theme.of(context).scaffoldBackgroundColor
+                              : Theme.of(context).colorScheme.background,
+                          child: Icon(
+                            Icons.more_vert_outlined,
+                            size: 20,
+                            color: hover.value ? textColor : textColor?.withOpacity(0.5),
+                          ),
+                        ),
+                      ),
+                    ),
+                    onHover: (value) => hover.value = value,
+                  );
+                }
 
   buildPasswordBoard(BuildContext context) {
     return ChangeNotifierProvider.value(
